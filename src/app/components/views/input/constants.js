da9b959e59230d925c98a7db1d306c678e36ad86
apid01 <apurvadhakre281@gmail.com>
2024-06-07 16:38:16 +0530

    Reusable Input Component
    Reusable Input Component

--- src/app/components/views/input/constants.js
+++ src/app/components/views/input/constants.js
@@ -1,9 +1,16 @@
 export const InputTypes = {
-    TEXT: 'text',
-    MULTILINE: 'multiline',
+  TEXT: "text",
+  MULTILINE: "multiline",
+  NUMBER: "number",
+  PASSWORD: "password",
+  EMAIL: "email",
+  SEARCH: "search",
 };
+export const InputTypeMuiTypeMap= {
+    
+}
 
 export const InputSizes = {
-    SMALL: 'small',
-    MEDIUM: 'medium',
-  };
+  SMALL: "small",
+  MEDIUM: "medium",
+};
